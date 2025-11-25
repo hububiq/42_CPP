347ae0321b90b2e3c27b546c7edd1ba8d746a8fd
hgatarek <hgatarek@DESKTOP-CJF1BA7.localdomain>
2025-11-25 18:02:21 +0100

    HumanA

--- module01/ex03/Weapon.hpp
+++ module01/ex03/Weapon.hpp
@@ -1,12 +1,13 @@
 #ifndef WEAPON_H
 # define WEAPON_H
+# include <string>
 
 class Weapon
 {
     private:
         std::string _type;
     public:
-        Weapon(st::string weaponType);
+        Weapon(std::string weaponType);
         ~Weapon();
 
         std::string const & getType();
