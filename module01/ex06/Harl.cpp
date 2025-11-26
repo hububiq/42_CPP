829ff7c06817f7442be72025d890568fc4054925
hgatarek <hgatarek@DESKTOP-CJF1BA7.localdomain>
2025-11-26 23:58:15 +0100

    last ex

---
+++ module01/ex06/Harl.cpp
@@ -0,0 +1,39 @@
+#include "Harl.hpp"
+
+Harl::Harl() {}
+
+void Harl::_debug()
+{
+    std::cout << "I love having extra bacon for" <<
+        "my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::_info()
+{
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn't put" <<
+            "enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+}
+
+void Harl::_warning()
+{
+    std::cout << "I think I deserve to have some extra bacon for free. I've been coming for" <<
+                "years, whereas you started working here just last month" << std::endl;
+}
+void Harl::_error()
+{
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::callFunction(int i)
+{
+    void (Harl::*ptrArray[])() = 
+    {
+        &Harl::_debug,    //taking addresses of each member function in class.
+        &Harl::_info,
+        &Harl::_warning,
+        &Harl::_error
+    };
+    (this->*ptrArray[i])();
+}
+
+Harl::~Harl() {}
