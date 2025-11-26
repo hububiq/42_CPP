bf68b6d8ec1c36be485ce0d1181761bd3bdda2b6
hgatarek <hgatarek@DESKTOP-CJF1BA7.localdomain>
2025-11-26 12:52:43 +0100

    Makefile and main

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -1,6 +1,23 @@
 #include <fstream>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-    
+    if (argc != 4)
+    {
+        std::cout << "Wrong input" << std::endl;
+        return (1);
+    }
+    std::ifstream filename = argv[1];
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+
+    std::ifstream srcFile("filename");
+    std::ofstream dstFile("filename.replace");
+    std::string = buffer;
+    while (std::getline(srcFile, buffer))
+    {
+        dstFile << buffer << std::endl;
+    }
+    return (0);
 }
