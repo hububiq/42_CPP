f8f48537e4d00636439bd1bf6f0d9f3d5ade16d7
hgatarek <hgatarek@DESKTOP-CJF1BA7.localdomain>
2025-12-09 19:29:28 +0100

    ex03 entirely

--- module03/ex03/DiamondTrap.hpp
+++ module03/ex03/DiamondTrap.hpp
@@ -2,9 +2,19 @@
 # define DIAMONDTRAP_H
 #include "FragTrap.hpp"
 
-class DiamondTrap 
+class DiamondTrap: public ScavTrap, public FragTrap                 //order would matter I guess, If we didnt assign members' values in constructor
 {
-    
+    private:
+        std::string _name;
+    public:
+        DiamondTrap();
+        DiamondTrap(const std::string& name);
+        DiamondTrap(const DiamondTrap& other);
+        DiamondTrap& operator=(const DiamondTrap& other);
+        ~DiamondTrap();
+
+        void attack(const std::string& target);
+        void whoAmI();
 };
 
 #endif
