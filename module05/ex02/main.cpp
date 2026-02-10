3b32e59112fc29b923701280f5570a6e9eb4c9c6
hgatarek <hgatarek@DESKTOP-CJF1BA7.localdomain>
2026-02-11 00:24:44 +0100

    ToFix:AFormConstructor

--- module05/ex02/main.cpp
+++ module05/ex02/main.cpp
@@ -2,6 +2,7 @@
 #include "AForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 
+/* its the best to call base class execute to handle common checks*/
 int main()
 {
 	ShrubberyCreationForm b("Pochlastac sie mozna");
