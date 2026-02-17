2a13240eb51ab736384a66884e1ea6a426c93f66
hgatarek <hgatarek@DESKTOP-CJF1BA7.localdomain>
2026-02-17 20:52:15 +0100

    Fixed:all

--- module06/ex00/ScalarConverter.cpp
+++ module06/ex00/ScalarConverter.cpp
@@ -21,7 +21,7 @@ void ScalarConverter::convert(std::string str)
     {
         if (str.length() == 1 && !std::isdigit(str[0]))
         {
-            if (str[0] < 32)
+            if (!std::isprint(str[0]))
                 std::cout << "Non displayable" << std::endl;
             else
                 std::cout << "char: " << str[0] << std::endl;
@@ -32,29 +32,38 @@ void ScalarConverter::convert(std::string str)
         else 
         {
             std::cout << std::fixed << std::setprecision(1);
-            if (int temp = std::stoi(str))
+            if (str.back() == 'f')
             {
+                float temp = std::stof(str);
                 if (temp > 31 && temp <= 255)
-                    std::cout << "char: " << static_cast<char>(temp) << std::endl;
+                    std::cout << "char od floata: " << static_cast<char>(temp) << std::endl;
                 else
-                    std::cout << "char: impossible" << std::endl;
+                    std::cout << "char od floata: Non displayable" << std::endl;
+                std::cout << "int: " << static_cast<int>(temp) << std::endl;
+                std::cout << "float: " << temp << "f" << std::endl;
+                std::cout << "double: " << static_cast<double>(temp) << std::endl;
+            }
+            else if (str.find('.') != std::string::npos)/* || (int temp = std::stof(str))*/
+            {
+                double temp = std::stod(str);
+                if (temp > 31 && temp <= 255)
+                    std::cout << "char od double: " << static_cast<char>(temp) << std::endl;
+                else
+                    std::cout << "char od double: Non displayable" << std::endl;
+                std::cout << "int: " << static_cast<int>(temp) << std::endl;
+                std::cout << "float: " << static_cast<float>(temp) << "f" << std::endl;
+                std::cout << "double: " << temp << std::endl;
+            }
+            else
+            {
+                int temp = std::stoi(str);
+                if (temp > 31 && temp <= 255)
+                    std::cout << "char od int: " << static_cast<char>(temp) << std::endl;
+                else
+                    std::cout << "char od int: Non displayable" << std::endl;
                 std::cout << "int: " << temp << std::endl;
                 std::cout << "float: " << static_cast<float>(temp) << "f" << std::endl;
                 std::cout << "double: " << static_cast<double>(temp) << std::endl;
-            }
-            else if (int temp = std::stod(str))/* || (int temp = std::stof(str))*/
-            {
-                std::cout << "char: impossible" << std::endl;
-                std::cout << "int: " << static_cast<int>(temp) << std::endl;
-                std::cout << "float: " << static_cast<float>(temp) << "f" << std::endl;
-                std::cout << "double: " << temp << std::endl;
-            }
-            else if (int temp = std::stof(str))
-            {
-                std::cout << "char: impossible" << std::endl;
-                std::cout << "int: " << static_cast<int>(temp) << std::endl;
-                std::cout << "float: " << static_cast<float>(temp) << "f" << std::endl;
-                std::cout << "double: " << temp << std::endl;
             }
         }
     }
