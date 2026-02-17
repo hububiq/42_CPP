85f780750f2c40888de920996eb7a7ded9724404
hgatarek <hgatarek@DESKTOP-CJF1BA7.localdomain>
2026-02-17 23:53:51 +0100

    Ex01:Done

---
+++ module06/ex01/Serializer.hpp
@@ -0,0 +1,24 @@
+#ifndef SERIALIZER_HPP
+# define SERIALIZER_HPP
+# include <cstdint> // or cstdint? (for uintptr_t)
+# include <iostream>
+# include <string>
+
+struct Data
+{
+    int nb;
+};
+
+class Serializer
+{
+    private:
+        Serializer();
+        Serializer(const Serializer& other);
+        Serializer& operator=(const Serializer& other);
+        ~Serializer();
+    public:
+        static uintptr_t serialize(Data* ptr);
+        static Data* deserialize(uintptr_t raw);
+};
+
+#endif
