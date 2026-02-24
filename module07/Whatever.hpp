e09bd92abbd528f6886da8c4d9447908b5f12459
hgatarek <hgatarek@DESKTOP-CJF1BA7.localdomain>
2026-02-24 18:46:08 +0100

    Done:ex00Templ

--- module07/Whatever.hpp
+++ module07/Whatever.hpp
@@ -2,7 +2,24 @@
 # define WHATEVER_HPP
 # include <iostream>
 
-//template for min(), max(), swap()
+template <typename T> T max(T x, T y)
+{
+    return (x > y) ? x : y;
+}
+
+template <typename T> T min(T x, T y)
+{
+    return (x < y) ? x : y;
+}
+
+template <typename T> void swap(T& x, T& y)    //reference - to operate on original variables to be able to print them
+{
+    T temp;
+
+    temp = x;
+    x = y;
+    y = temp;
+}
 
 class Whatever
 {
@@ -11,7 +28,5 @@ class Whatever
     Whatever& operator=(const Whatever& other);
     ~Whatever();
 };
-
-//overloads for comparisons operators
 
 #endif 
