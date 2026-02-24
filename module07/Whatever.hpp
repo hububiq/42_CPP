1fa449d1e18252bc15edbc2371918eea9720f202
hgatarek <hgatarek@DESKTOP-CJF1BA7.localdomain>
2026-02-24 13:02:54 +0100

    FilesOrder

---
+++ module07/Whatever.hpp
@@ -0,0 +1,17 @@
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+# include <iostream>
+
+//template for min(), max(), swap()
+
+class Whatever
+{
+    Whatever();
+    Whatever(const Whatever& other);
+    Whatever& operator=(const Whatever& other);
+    ~Whatever();
+};
+
+//overloads for comparisons operators
+
+#endif 
---
+++ module07/main.cpp
@@ -0,0 +1,6 @@
+#include "Whatever.hpp"
+
+int main()
+{
+    return 0;
+}
