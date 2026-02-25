7c54fac62680b25ef3599dca4de4a0d05942bf99
hgatarek <hgatarek@DESKTOP-CJF1BA7.localdomain>
2026-02-25 22:14:31 +0100

    FixedConstnessAmbiguity

--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -1,10 +1,12 @@
 #include "Iter.hpp"
 
+/* const and non-const is assured with T*/
 int main()
 {
-    // int ar[3] = {3, 5, 2};
+    const int ar[3] = {3, 5, 2};
     double arr[3] = {3.2, 5.1, 2.4};
-    ::iter(arr, 3, ::multiply<double>); //casting type to template, doest it have to be the same as array content?
+    ::iter(arr, 3, ::multiply<double>); //without scope resolution operator also all good. no ambiguity with inubilt funcs.
     std::cout << arr[0] << " " << arr[1] << " " << arr[2] << std::endl;
+    ::iter(ar, 3, ::print<const int>);
     return 0;
 }
