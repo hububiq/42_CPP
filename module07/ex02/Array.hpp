fff9aa2ee9278a6f455fce6fcd5060fbe24bc4ee
Hubert Gatarek <hgatarek@c1r8s2.42warsaw.pl>
2026-02-26 14:59:16 +0100

    ToFix:OutOfRange

--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -2,20 +2,24 @@
 # define ARRAY_HPP
 # include <iostream>
 # include <string>
-# include "Array.tpp"
+# include <cstddef>
+# include <exception>
 
 template <typename T>
 class Array 
 {
     private:
-        T* _array;
-        unsigned int _size;
+        T* 				_array;
+        unsigned int 	_size;
     public:
         Array();
         Array(unsigned int n);
-
+		Array(const Array& other);
+		Array& operator=(const Array& other);
         ~Array();
-
+		T getValue(unsigned int i);
+		//size member function
 };
 
+# include "Array.tpp"
 #endif
