d888cd0b926581d86afbdd0db79b66dc79dbfc07
hgatarek <hgatarek@DESKTOP-CJF1BA7.localdomain>
2026-02-28 01:08:56 +0100

    SmallFix&Test

--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -3,15 +3,18 @@
 
 int main()
 {
-	//int* a = new int(); //default init to 
-	Array<int> b(4);
+	int* a = new int(); //default init to 0
+	std::cout << *a << std::endl;
+	Array<int> b(5);
 	try
 	{
-		std::cout << b[5] << std::endl;
+		std::cout << b[0] << std::endl;
 	}
 	catch (std::exception& e)
 	{
 		std::cout << e.what() << std::endl;
 	}
+	for (int i = 0; i < b.size(); i++)
+		std::cout << b[i] << std::endl;
     return 0;
 }
