e15cc4482c8792da72605a03bf4fe8f7e697b07f
hgatarek <hgatarek@DESKTOP-CJF1BA7.localdomain>
2026-03-10 23:54:08 +0100

    Done:InputParse

--- module09/ex00/main.cpp
+++ module09/ex00/main.cpp
@@ -1,8 +1,33 @@
 #include "BitcoinExchange.hpp"
 
+std::map<std::string, double> processDb(std::ifstream& Db)
+{
+	std::string line;
+	std::map<std::string, double> Map;
+
+	while (std::getline(Db, line))
+	{
+		size_t index = line.find(",");
+		std::string date = line.substr(0, index);
+		double price = std::atof(line.substr(index + 1).c_str());
+		Map[date] = price;
+	}
+	return Map;
+}
+
 int main(int argc, char **argv)
 {
-	fstream DbFile("data.csv");
-	while (getline)
+	(void)argv;
+	if (argc != 2)
+		return std::cerr << "Arguments needed" << std::endl, -1;
+	std::ifstream DbFile("data.csv");
+	if (!DbFile.is_open())
+		return std::cerr << "Couldn't open such file" << std::endl, -1;
+	std::map<std::string, double> priceMap = processDb(DbFile);
+	// for (std::map<std::string, double>::const_iterator it = priceMap.begin(); it != priceMap.end(); it++)
+		// std::cout << it->first << " " << it->second << std::endl;
+
+	/* function to pass Map to calculation and input.txt */
+	// std::ifstream file(name.c_str());  //ofstream construction with std::string converted to char *
 	return 0;
 }
