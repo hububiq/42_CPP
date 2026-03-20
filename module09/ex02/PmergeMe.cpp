39f047f96cd9bcfcaed77e04a4137c9e45eef250
hgatarek <hgatarek@DESKTOP-CJF1BA7.localdomain>
2026-03-20 18:19:01 +0100

    Main/PendPrep

--- module09/ex02/PmergeMe.cpp
+++ module09/ex02/PmergeMe.cpp
@@ -60,6 +60,53 @@ void PmergeMe::_algo(std::vector<int>& vec, int blockSize, int recStep)
         std::cout << vec[i] << " ";
     std::cout << std::endl;
     _algo(vec, blockSize * 2, recStep + 1);
+    std::cout << "Only here creating the main and pend in step:" << recStep << std::endl;
+    // std::cout << vec[blockSize - 1] << std::endl;
+    std::vector<std::vector<int>> mainChain;
+    std::vector<std::vector<int>> pendChain;
+    for (int i = 1; i < elements; i++)
+    {
+        if (i % 2 != 0)
+        {
+            std::vector<int> tempPend;
+            for (int j = 0; j < blockSize; j++)
+            {
+                tempPend.push_back(vec[i * j]);
+            }
+            pendChain.push_back(tempPend);
+        }
+        else if (i % 2 == 0)
+        {
+            std::vector<int> tempMain;
+            for (int j = 0; j < blockSize; j++)
+            {
+                tempMain.push_back(vec[i * j]);
+            }
+            mainChain.push_back(tempMain);
+        }
+    }
+    for (int i = 0; i < mainChain.size(); i++)
+    {
+        for (int j = 0; j < mainChain[i].size(); j++)
+            std::cout << mainChain[i][j] << " ";
+        std::cout << "TO byl main" << std::endl;
+    }
+    // finish making main and pend
+    // create more generic logic and push to main with a helper or pushing straitaway the first loser to frontal main
+
+
+    //generate jacobstahl
+    //insert from pend to main based on indexes in pend compared to jacobsthal from lower to higher
+    //remember about "strugglers"
+    //split with helpers and objectify main algo scope
+    //add time measurement
+
+    // for (int i = 0; i < pendChain.size(); i++)
+    // {
+    //     for (int j = 0; j < pendChain[i].size(); j++)
+    //         std::cout << pendChain[i][j] << " ";
+    //     std::cout << "TO byl pend" << std::endl;
+    // }
 }
 
 void PmergeMe::executeAlgo()
