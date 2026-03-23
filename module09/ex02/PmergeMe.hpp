1dc8287fc5a55b72d0a555d794db02b7c4c69d1f
hgatarek <hgatarek@DESKTOP-CJF1BA7.localdomain>
2026-03-23 23:48:51 +0100

    Done:AlgoforDeque

--- module09/ex02/PmergeMe.hpp
+++ module09/ex02/PmergeMe.hpp
@@ -4,35 +4,250 @@
 # include <string>
 # include <stdexcept>
 # include <vector>
+# include <deque>
 # include <sstream>
 # include <algorithm> //for begin(), end() iterator, std::min()
 # include <ctime>
 # include <iomanip>
 
+
+template <typename T>
 class PmergeMe
 {
     private:
-        std::vector<int> _vec;
-        void _algo(std::vector<int>& vec, int blockSize, int recStep);                          //private for work, public function just for starter
+        T _container;
+        typedef std::vector<T> dbVec;
+        typedef std::deque<T> dbDeq;
+        template <typename D>
+        void _algo(T& container, int blockSize, int recStep);                          //private for work, public function just for starter
     public:
-        typedef std::vector<std::vector<int>> dbVec;
         PmergeMe();
+        // PmergeMe(ContainerType type);
         PmergeMe(const PmergeMe& other);
         PmergeMe& operator=(const PmergeMe& other);
         ~PmergeMe();
 
-        std::vector<int> getVector();
+        T getVector();
         void pushNumber(int val);
         void validateInput(std::stringstream& ss);
+        template <typename D>
         void executeAlgo();
-        void jacobNumbersInsertion(dbVec& mainChain, dbVec& pendChain);
-        void strugglerInsert(dbVec& mainChain, std::vector<int>& struggler);
-        void rewriteVec(std::vector<int>& vec, dbVec& mainChain);
-        void strugglerCut(std::vector<int>& struggler, bool& hasStruggler, std::vector<int>& vec, int blockSize);
-        void elemWinnersCompare(int elements, int blockSize, std::vector<int>& vec);
-        void initMainPend(std::vector<int>& vec, int blockSize, int elements, dbVec& mainChain, dbVec& pendChain);
-        std::vector<int> generateJakobstahl(int elements);
-        static bool compare(const std::vector<int>& a, const std::vector<int>& b);              //static for lower_bound compliance   
+        template <typename D>
+        void jacobNumbersInsertion(D& mainChain, D& pendChain);
+        template <typename D>
+        void strugglerInsert(D& mainChain, T& struggler);
+        template <typename D>
+        void rewriteVec(T& cont, D& mainChain);
+        void strugglerCut(T& struggler, bool& hasStruggler, T& cont, int blockSize);
+        void elemWinnersCompare(int elements, int blockSize, T& cont);
+        template <typename D>
+        void initMainPend(T& cont, int blockSize, int elements, D& mainChain, D& pendChain);
+        T generateJakobstahl(int elements);
+        static bool compare(const T& a, const T& b);              //static for lower_bound compliance   
 };
+
+template <typename T>
+PmergeMe<T>::PmergeMe() {}
+
+template <typename T>
+PmergeMe<T>::PmergeMe(const PmergeMe& other)
+{
+    this->_container = other._container;
+}
+
+template <typename T>
+PmergeMe<T>& PmergeMe<T>::operator=(const PmergeMe& other)
+{
+    if (this != &other)
+    {
+        this->_container = other._container;
+    }
+    return *this;
+}
+
+template <typename T>
+PmergeMe<T>::~PmergeMe() {}
+
+template <typename T>
+T PmergeMe<T>::getVector()
+{
+    return this->_container;
+}
+
+template <typename T>
+void PmergeMe<T>::pushNumber(int val)
+{
+    this->_container.push_back(val);
+}
+
+template <typename T>
+void PmergeMe<T>::validateInput(std::stringstream& ss)
+{
+    std::string token;
+
+    while(ss >> token)
+    {
+        for (int i = 0; i < token.size(); i++)
+        {
+            if (!std::isdigit(token[i]))                                    //handle also minus
+                throw std::invalid_argument("Wrong input.");
+        }
+        if (std::find(this->_container.begin(), this->_container.end(), std::stoi(token)) != this->_container.end())
+                throw std::invalid_argument("Duplicates.");
+        else
+            this->pushNumber(std::stoi(token));
+    }
+}
+
+template <typename T>
+bool PmergeMe<T>::compare(const T& a, const T& b)
+{
+    return a.back() < b.back();
+}
+
+template <typename T>
+template <typename D>
+void PmergeMe<T>::jacobNumbersInsertion(D& mainChain, D& pendChain)
+{
+    T jN = generateJakobstahl(pendChain.size());
+    int inserted = 1;
+    int lastPushedIndex = -1;
+    for (int i = 3; i < jN.size(); i++)
+    {
+        int currentJacob = jN[i];
+        int startIndex = std::min((int)pendChain.size() - 1, currentJacob - 2);             //safety for not going out of pend if its too short comparing to Jakobstahl number
+        for (int j = startIndex; j > lastPushedIndex; j--)                                  //taking lower and lower b from pendChain
+        {
+            int pairNum = j + 2;                                                            //reverse logic to pair up to main, which in order
+            int limit = pairNum + inserted - 1;
+            int actualLimit = std::min((int)mainChain.size(), limit);                       //cast because min takes both types the same
+            typename D::iterator it = std::lower_bound(
+                mainChain.begin(), 
+                mainChain.begin() + actualLimit, 
+                pendChain[j], 
+                compare);                                                                   //pointer to function
+            mainChain.insert(it, pendChain[j]);
+            inserted++;
+        }
+        lastPushedIndex = startIndex;
+        if (lastPushedIndex >= (int)pendChain.size() - 1)
+            break;
+    }
+}
+
+template <typename T>
+template <typename D>
+void PmergeMe<T>::strugglerInsert(D& mainChain, T& struggler)
+{
+    typename D::iterator it = std::lower_bound(
+             mainChain.begin(), 
+             mainChain.end(), 
+             struggler, 
+             compare);
+    mainChain.insert(it, struggler);
+}
+
+template <typename T>
+template <typename D>
+void PmergeMe<T>::rewriteVec(T& cont, D& mainChain)
+{
+    cont.clear(); 
+    for (size_t i = 0; i < mainChain.size(); i++) 
+    {
+        for (size_t j = 0; j < mainChain[i].size(); j++) 
+            cont.push_back(mainChain[i][j]);
+    }
+}
+
+template <typename T>
+void PmergeMe<T>::strugglerCut(T& struggler, bool& hasStruggler, T& cont, int blockSize)
+{
+     int start = cont.size() - blockSize;             //cutting out the struggler
+        for (int k = 0; k < blockSize; k++) 
+            struggler.push_back(cont[start + k]);
+        cont.erase(cont.begin() + start, cont.end());
+        hasStruggler = true;
+}
+
+template <typename T>
+void PmergeMe<T>::elemWinnersCompare(int elements, int blockSize, T& cont)
+{
+    for (int i = 1; i < elements; i += 2)               //comparing elements
+    {
+        int rightWinner = (i + 1) * blockSize - 1;
+        int leftWinner = i * blockSize - 1;
+        if (cont[rightWinner] < cont[leftWinner])
+        {
+            for (int j = 0; j < blockSize; j++)
+                std::swap(cont[rightWinner - j], cont[leftWinner - j]);
+        }
+    }
+}
+
+template <typename T>
+template <typename D>
+void PmergeMe<T>::initMainPend(T& cont, int blockSize, int elements, D& mainChain, D& pendChain)
+{
+    T b1(cont.begin(), cont.begin() + blockSize);
+    T a1(cont.begin() + blockSize, cont.begin() + 2 * blockSize);
+    mainChain.push_back(b1);
+    mainChain.push_back(a1);
+    for (int i = 2; i < elements - 1; i += 2)
+    {
+        T bBlock(cont.begin() + i * blockSize, cont.begin() + (i + 1) * blockSize);
+        pendChain.push_back(bBlock);
+        T aBlock(cont.begin() + (i + 1) * blockSize, cont.begin() + (i + 2) * blockSize);
+        mainChain.push_back(aBlock);
+    }
+}
+
+template <typename T>
+T PmergeMe<T>::generateJakobstahl(int elements)
+{
+    T jakobNumbers;
+    jakobNumbers.push_back(0);
+    jakobNumbers.push_back(1);
+    int i = 2;
+    while (true)
+    {
+        int jakobNumb = jakobNumbers[i - 1] + 2 * jakobNumbers[i - 2];
+        jakobNumbers.push_back(jakobNumb);
+        if (jakobNumb > elements + 1)
+            break;
+        i++;
+    }
+    return jakobNumbers;
+}
+
+template <typename T>
+template <typename D>
+void PmergeMe<T>::_algo(T& container, int blockSize, int recStep)
+{
+    T struggler;
+    bool hasStruggler = false;
+    D mainChain;
+    D pendChain;
+
+    int elements = container.size() / blockSize;
+    if (elements < 2)
+        return ;
+    if (elements % 2 != 0) 
+        this->strugglerCut(struggler, hasStruggler, container, blockSize);
+    this->elemWinnersCompare(elements, blockSize, container);
+    this->template _algo<D>(container, blockSize * 2, recStep + 1);
+    this->initMainPend(container, blockSize, elements, mainChain, pendChain);
+    if (!pendChain.empty())
+        this->jacobNumbersInsertion(mainChain, pendChain);
+    if (hasStruggler) 
+        this->strugglerInsert(mainChain, struggler);
+    this->rewriteVec(container, mainChain);
+}
+
+template <typename T>
+template <typename D>
+void PmergeMe<T>::executeAlgo()                 
+{
+    this->template _algo<D>(this->_container, 1, 1);        //C++ requires "template" keyword because _algo is a dependent template
+}
 
 #endif
--- module09/ex02/main.cpp
+++ module09/ex02/main.cpp
@@ -1,14 +1,17 @@
 #include "PmergeMe.hpp"
 
 //looping with for is quick on i.e. 3000 numbers. new/malloc or creating subvectors will take processor time for memory management
-//vector is continuous segments, deque is separated segments but still fast, probably faster for bigger inputs
+//vector is continuous segments, deque is separated continuous segments but even fast, probably faster for bigger inputs
 
 int main(int argc, char **argv)
 {
-    PmergeMe obj;
+    PmergeMe< std::vector<int> > objV;
+    PmergeMe< std::deque<int> > objD;
     std::stringstream ss;
-    clock_t before;
-    clock_t after;
+    clock_t beforeV;
+    clock_t afterV;
+    clock_t beforeD;
+    clock_t afterD;
 
     if (argc == 2)
         return std::cout << "Nothing to sort." << std::endl, 0;
@@ -16,10 +19,18 @@ int main(int argc, char **argv)
         ss << argv[i] << " ";
     try 
     {
-        before = clock();
-        obj.validateInput(ss);
-        obj.executeAlgo();
-        after = clock();
+        objV.validateInput(ss);
+        std::vector<int> temp = objV.getVector();  //will copy because string stream is empty now.
+        for (size_t i = 0; i < temp.size(); i++)
+            objD.pushNumber(temp[i]);
+        beforeV = clock();
+        objV.executeAlgo< std::vector< std::vector<int> > >();
+        afterV = clock();
+        objD.validateInput(ss);
+        beforeD = clock();
+        objD.executeAlgo< std::deque< std::deque<int> > >();
+        afterD = clock();
+
     }
     catch (std::invalid_argument& e)
     {
@@ -30,12 +41,14 @@ int main(int argc, char **argv)
     for (int i = 1; i < argc; i++)
         std::cout << argv[i] << " ";
     std::cout << std::endl;
-    std::vector<int> vecShow = obj.getVector();
+    std::vector<int> vecShow = objV.getVector();
     std::cout << "After:   ";
     for (int i = 0; i < argc - 1; i++)
         std::cout << vecShow[i] << " ";
     std::cout << std::endl;
-    std::cout << "Time to process a range of " << argc - 1 << " arguments with std::" << "vector is : " <<           //solve container with enum?
-    std::fixed << std::setprecision(4) << (float)(after - before) / CLOCKS_PER_SEC * 1000000 << " us" << std::endl;
+    std::cout << "Time to process a range of " << argc - 1 << " arguments with std::" << "vector is : " <<              //should it be more generic?
+    std::fixed << std::setprecision(1) << (float)(afterV - beforeV) / CLOCKS_PER_SEC * 1000000 << " us" << std::endl;
+    std::cout << "Time to process a range of " << argc - 1 << " arguments with std::" << "deque is : " <<           
+    std::fixed << std::setprecision(1) << (float)(afterD - beforeD) / CLOCKS_PER_SEC * 1000000 << " us" << std::endl;
     return 0;
 }
